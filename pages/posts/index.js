b56926c2a1bc0145603690f40f6674cbf50fa02f
Maky <franklinjaradev@gmail.com>
2021-08-02 23:38:27 -0400

    Scss and body for posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,17 @@
-import React from "react";
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POST = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with python",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a great framework ready for production",
+    date: "2022-02-10",
+  },
+];
 
 const PostsPage = () => {
-  return <div></div>;
+  return <AllPosts posts={DUMMY_POST} />;
 };
 
 export default PostsPage;
