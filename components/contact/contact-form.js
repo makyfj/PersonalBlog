67345da5138387f32eb709c209f0112ab8eaa9d7
Maky <franklinjaradev@gmail.com>
2021-08-08 19:43:35 -0400

    Contact API

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,28 +1,69 @@
+import { useState } from "react";
 import classes from "./contact-form.module.scss";
 
 const ContactForm = () => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  const sendMessageHandler = (e) => {
+    e.preventDefault();
+
+    // add client-side validation
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <div className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(e) => setEnteredEmail(e.target.value)}
+            />
           </div>
           <div className={classes.control}>
-            <label htmlFor="email">Your Name</label>
-            <input type="text" id="name" required />
+            <label htmlFor="name">Your Name</label>
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(e) => setEnteredName(e.target.value)}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="message">Your Message</label>
-            <textarea id="message" required row="5"></textarea>
+            <textarea
+              id="message"
+              required
+              row="5"
+              value={enteredMessage}
+              onChange={(e) => setEnteredMessage(e.target.value)}
+            />
           </div>
           <div className={classes.actions}>
             <button>Send Message</button>
           </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
