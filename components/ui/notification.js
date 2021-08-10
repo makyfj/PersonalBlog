312b1a3d18e7873e548c7d4ceee31c7c560fc856
Maky <franklinjaradev@gmail.com>
2021-08-09 20:04:52 -0400

    Added react portals to notification

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,3 +1,4 @@
+import ReactDOM from "react-dom";
 import classes from "./notification.module.scss";
 
 const Notification = ({ title, message, status }) => {
@@ -13,11 +14,12 @@ const Notification = ({ title, message, status }) => {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById("notifications")
   );
 };
 
